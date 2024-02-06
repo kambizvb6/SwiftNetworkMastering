a0c220d150e62f6d440c1ad0e7f259adf00b7d4c
Kambiz Dehghani <kambizvb6@gmail.com>
2024-02-06 00:54:52 -0600

    Initial Commit

---
+++ SwiftNetworkMastering/SwiftNetworkMasteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNetworkMasteringApp.swift
+//  SwiftNetworkMastering
+//
+//  Created by Kambiz on 2024-02-06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNetworkMasteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
